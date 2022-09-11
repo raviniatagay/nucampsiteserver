00d45621555f14fa8b5f46c4fc138366a44f077f
raviniatagay <raviniatagay@yahoo.com>
2022-09-10 19:54:25 -0500

    Wk3 Assignment

--- nucampsiteServer/routes/users.js
+++ nucampsiteServer/routes/users.js
@@ -7,8 +7,14 @@ const user = require('../models/user');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find()
+    .then(users => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    })
+    .catch(err => next(err));
 });
 
 router.post('/signup', (req, res) => {
