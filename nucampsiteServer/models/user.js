f677c7d7f1953517d24ef21439dbd4d830660dbf
raviniatagay <raviniatagay@yahoo.com>
2022-09-14 10:35:59 -0500

    Passport Facebook

--- nucampsiteServer/models/user.js
+++ nucampsiteServer/models/user.js
@@ -11,10 +11,11 @@ const userSchema = new Schema({
         type: String,
         default: ''
     },
+    facebookId: String,
     admin: {
         type: Boolean,
         default: false
-    }
+    }    
 });
 
 userSchema.plugin(passportLocalMongoose);
